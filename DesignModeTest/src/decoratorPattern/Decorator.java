5178a9a5e5dd71d032fd6c48c903dc0f55cd0219
lh <2273133714@qq.com>
2019-04-12 14:06:12 +0800

    test

--- DesignModeTest/src/decoratorPattern/Decorator.java
+++ DesignModeTest/src/decoratorPattern/Decorator.java
@@ -1,6 +1,6 @@
 package decoratorPattern;
 
-public abstract class Decorator extends Component{
+public class Decorator extends Component{
     protected Component component;
     public Decorator(Component component){
         this.component=component;
